c0014243d9424bfd1f4d4d23b64eeee64c4a599b
kimcy929 <kimcy92@gmail.com>
2018-03-08 23:42:14 +0700

    Improve check runtime permission

--- quicksave/src/main/java/com/example/quicksave/utils/PermissionHelper.kt
+++ quicksave/src/main/java/com/example/quicksave/utils/PermissionHelper.kt
@@ -1,25 +1,35 @@
 package com.example.quicksave.utils
 
-import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.support.v4.app.ActivityCompat
 
 /**
  * Created by kimcy929 on 3/8/2018.
+ *
+ * https://developer.android.com/training/permissions/requesting.html#kotlin
+ *
  */
+
 object PermissionHelper {
 
-    private const val PERMISSION_REQUEST = 1
+    const val PERMISSION_REQUEST = 1
 
     fun checkPermission(permissions: Array<String> = arrayOf(), activity: Activity) {
 
         permissions.forEach {
-            if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(activity, it) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(activity, arrayOf(it), PERMISSION_REQUEST)
             }
         }
+    }
 
+    fun permissionGranted(permissions: Array<String> = arrayOf(), activity: Activity): Boolean {
+        permissions.forEach {
+            if (ActivityCompat.checkSelfPermission(activity, it) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
     }
 }
